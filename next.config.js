03cd0e7813c5764889cf7da273c195387670006e
devzoofi <zoozoofifi@outlook.com>
2024-08-13 16:13:00 +0800

    adapter gh-pages

--- next.config.js
+++ next.config.js
@@ -1,11 +1,14 @@
+const basePath = process.env.BASE_PATH || ''
+console.info('BASE_PATH', basePath)
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  basePath,
   webpack: (config) => {
     config.resolve.fallback = { fs: false, net: false, tls: false }
     return config
   },
-  images: { unoptimized: true }
+  images: { unoptimized: true },
 }
 
 module.exports = nextConfig
