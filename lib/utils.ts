ef689feee744bd5311a71f9123e02b0ac065c5e8
devzoofi <zoozoofifi@outlook.com>
2024-09-09 15:25:56 +0800

    [beta] fix fmt time

--- lib/utils.ts
+++ lib/utils.ts
@@ -132,6 +132,7 @@ export const fmtTime = (
 const FMT_DURATION_TYPES = ['seconds', 'minutes', 'hours', 'days', 'weeks', 'months', 'years'] as const
 type FMT_DURATION_TYPE = (typeof FMT_DURATION_TYPES)[number]
 export const fmtDuration = (duration: number | bigint, type: FMT_DURATION_TYPE | 'auto' = 'auto') => {
+  duration < 0 && (duration = 0)
   const durationBn = typeof duration == 'number' ? BigInt(duration) : duration
   const divVauleMap: { [k in FMT_DURATION_TYPE]: bigint } = {
     seconds: 1000n,
